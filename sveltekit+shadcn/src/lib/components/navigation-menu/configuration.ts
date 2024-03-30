cc533bfa9fdbd9e192f14b61235a4823f6176942
GantonL <45093712+GantonL@users.noreply.github.com>
2024-03-30 17:05:14 +0300

    added error boundry and example page and link

--- sveltekit+shadcn/src/lib/components/navigation-menu/configuration.ts
+++ sveltekit+shadcn/src/lib/components/navigation-menu/configuration.ts
@@ -1,9 +1,10 @@
 import type { NavigationLink } from "$lib/models/navigation-link";
-import {Home, Banana, Settings } from "lucide-svelte";
+import {Home, Banana, Settings, Ban } from "lucide-svelte";
 
 export const NavigationItemsConfiguration: NavigationLink[] = [
   { label: 'Home', icon: Home, link: '/' },
   { label: 'Example', icon: Banana, link: '/example' },
+  { label: 'Error', icon: Ban, link: '/error' },
 ];
 
 export const MoreNavigationItems: NavigationLink[] = [
---
+++ sveltekit+shadcn/src/routes/+error.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+</script>
+<div class="flex items-center justify-center h-80">
+  {#if $page?.error}
+      <div class="flex flex-col gap-4 border border-destructive/50 bg-accent/50 text-destructive-foreground rounded-lg p-8">
+          <h1 class="text-xl">
+            Oh no! 
+            {#if $page?.status === 404}
+              This page probably doesn't exist.
+            {:else}
+              Something went wrong.
+            {/if} 
+          </h1>
+          <section class="text-destructive-foreground/60">
+            {#if $page?.status}
+                <p>Error Code: {$page?.status}</p>
+            {/if}
+            {#if $page?.error?.message}
+                <p>Details: {$page?.error?.message}</p>
+            {/if}
+          </section>
+      </div>
+  {/if}
+</div>
