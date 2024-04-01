2d9fdfabeff14eecd54d9d91a0770c2d953caf42
GantonL <45093712+GantonL@users.noreply.github.com>
2024-04-01 06:59:29 +0300

    added seo robots and twitter card meta tags

--- sveltekit+shadcn/src/lib/components/seo/seo.svelte
+++ sveltekit+shadcn/src/lib/components/seo/seo.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { AppName, BaseUrl } from "$lib/consts";
+	import { AppName, Author, BaseUrl, TwitterSitename, TwitterUsername } from "$lib/consts";
 
   export let title: string;
   export let description: string;
@@ -14,10 +14,13 @@
 <svelte:head>
   <title>{title ? `${title} | `: ''}{AppName}</title>
   <link rel="canonical" href={`${BaseUrl}${slug}`} />
+  <meta name="robots" content="index, follow" />
+  <meta name="author" content={Author} />
   
   {#if description}
     <meta name="description" content={description} />
     <meta property="og:description" content={description} />
+    <meta name="twitter:description" content={description} />
   {/if}
   
   <meta property="og:title" content={title ? `${title} | ${AppName}`: AppName} />
@@ -26,11 +29,18 @@
   <meta property="og:url" content={`${BaseUrl}${slug}`} />
   <meta property="og:type" content={'website'} />
 
+  <meta name="twitter:card" content={!image && !squareImage ? "summary" : "summary_large_image"} />
+  <meta name="twitter:title" content={title ? `${title} | ${AppName}`: AppName} />
+  <meta name="twitter:site" content={TwitterSitename} />
+  <meta name="twitter:creator" content={TwitterUsername} />
+
   {#if image?.url}
     <meta property="og:image" content={image.url} />
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="627" />
     <meta property="og:image:alt" content={image.alt} />
+    <meta name="twitter:image" content={image.url} />
+    <meta name="twitter:image:alt" content={image.alt} />
   {/if}
 
   {#if squareImage?.url}
@@ -38,5 +48,9 @@
     <meta property="og:image:width" content="400" />
     <meta property="og:image:height" content="400" />
     <meta property="og:image:alt" content={squareImage.alt} />
+    <meta name="twitter:image" content={squareImage.url} />
+    <meta name="twitter:image:alt" content={squareImage.alt} />
   {/if}
+
+
 </svelte:head>
--- sveltekit+shadcn/src/lib/consts.ts
+++ sveltekit+shadcn/src/lib/consts.ts
@@ -1,2 +1,5 @@
 export const AppName: string = 'My Brand';
 export const BaseUrl: string = 'https://my-brand.com';
+export const Author: string = 'Guy Lahav';
+export const TwitterUsername: string = '@myusername';
+export const TwitterSitename: string = '@mysitename';
