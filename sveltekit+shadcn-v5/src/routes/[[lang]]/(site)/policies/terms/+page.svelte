7ea69318fbfc16300ac94b93eb11fd52a30a2a89
Guy Lahav <guylahav@machine.local>
2025-07-08 08:51:30 +0300

    Added site markdown resources pages

--- sveltekit+shadcn-v5/src/routes/[[lang]]/(site)/policies/terms/+page.svelte
+++ sveltekit+shadcn-v5/src/routes/[[lang]]/(site)/policies/terms/+page.svelte
@@ -2,7 +2,7 @@
 	import ResourceMarkdown from '$lib/components/resource-markdown/resource-markdown.svelte';
 	import { t } from '$lib/i18n';
 	import { title } from '$lib/stores';
-	title.set(t.get('common.cookie_policy'));
+	title.set(t.get('common.terms_of_service'));
 </script>
 
-<ResourceMarkdown path="cookies-policy" />
+<ResourceMarkdown path="site-terms" />
