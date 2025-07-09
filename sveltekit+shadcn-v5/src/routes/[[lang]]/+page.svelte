884e7907158c13f8a067089dd206df34d3e22850
Guy Lahav <guylahav@machine.local>
2025-07-09 08:43:28 +0300

    Added homepage markdown

--- sveltekit+shadcn-v5/src/routes/[[lang]]/+page.svelte
+++ sveltekit+shadcn-v5/src/routes/[[lang]]/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+	import ResourceMarkdown from '$lib/components/resource-markdown/resource-markdown.svelte';
 	import { title } from '$lib/stores';
 
 	title.clear();
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<ResourceMarkdown path="homepage" />
