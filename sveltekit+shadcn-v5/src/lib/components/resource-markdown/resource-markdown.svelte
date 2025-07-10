157691d7c5e26f3b18bbf3eba9ac1f3941d45d7e
Guy Lahav <guylahav@machine.local>
2025-07-10 08:28:22 +0300

    Support tailwind typography

--- sveltekit+shadcn-v5/src/lib/components/resource-markdown/resource-markdown.svelte
+++ sveltekit+shadcn-v5/src/lib/components/resource-markdown/resource-markdown.svelte
@@ -10,7 +10,9 @@
 	});
 </script>
 
-<article class="prose text-secondary-foreground text-justify">
+<article
+	class="prose prose-strong:text-secondary-foreground prose-a:text-secondary-foreground prose-headings:text-secondary-foreground text-secondary-foreground text-justify"
+>
 	{#if readyToRender}
 		<Content />
 	{/if}
