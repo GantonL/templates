2444bab0e0dd05bd0f61bb280f075f31afa038c0
Guy Lahav <guylahav@machine.local>
2025-07-12 02:33:29 +0300

    Added cookie preferences banner

--- sveltekit+shadcn-v5/src/routes/[[lang]]/+layout.svelte
+++ sveltekit+shadcn-v5/src/routes/[[lang]]/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import Shell from '$lib/components/shell/shell.svelte';
+	import CookiesPreferencesBanner from '$lib/components/cookies-preferences-banner/cookies-preferences-banner.svelte';
 
 	let { children } = $props();
 </script>
 
+<CookiesPreferencesBanner />
 <Shell>
 	{@render children?.()}
 </Shell>
