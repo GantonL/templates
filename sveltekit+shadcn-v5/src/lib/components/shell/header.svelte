154e49524ed7fb9435c6329b34da6aebd27b1326
Guy Lahav <guylahav@machine.local>
2025-07-13 05:06:41 +0300

    Header component with theme switcher

--- sveltekit+shadcn-v5/src/lib/components/shell/header.svelte
+++ sveltekit+shadcn-v5/src/lib/components/shell/header.svelte
@@ -1,4 +1,13 @@
 <script lang="ts">
+	import { AppName } from '$lib/api/configurations/common';
+	import ThemeSwitcher from '../theme-switcher/theme-switcher.svelte';
 </script>
 
-<header></header>
+<header class="grid w-full grid-cols-2 border-b p-2">
+	<div class="flex flex-row items-center gap-4">
+		<a href="/" class="self-center text-2xl font-extrabold">{AppName}</a>
+	</div>
+	<div class="flex flex-row justify-end gap-2">
+		<ThemeSwitcher />
+	</div>
+</header>
---
+++ sveltekit+shadcn-v5/src/lib/components/theme-switcher/theme-switcher.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Moon, Sun } from '@lucide/svelte';
+	import { toggleMode } from 'mode-watcher';
+</script>
+
+<Button onclick={toggleMode} variant="outline" size="icon" class="aspect-square">
+	<Sun
+		class="h-[1.2rem] w-[1.2rem] scale-0 rotate-0 transition-all dark:scale-100 dark:-rotate-90"
+	/>
+	<Moon
+		class="absolute h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:rotate-90"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
