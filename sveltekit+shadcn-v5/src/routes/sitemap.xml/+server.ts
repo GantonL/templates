9fdd2405d2b8b1e3e7ee07effaaea7db410d5467
Guy Lahav <guylahav@machine.local>
2025-07-18 19:37:44 +0300

    Improved sitemap

--- sveltekit+shadcn-v5/src/routes/sitemap.xml/+server.ts
+++ sveltekit+shadcn-v5/src/routes/sitemap.xml/+server.ts
@@ -1,24 +1,54 @@
-export async function GET() {	
-  return new Response(
-    `<?xml version="1.0" encoding="UTF-8" ?>
-    <urlset  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
-      xmlns:xhtml="http://www.w3.org/1999/xhtml"
-      xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+import { BaseUrl } from '$lib/api/configurations/common';
+import { AvailableLocals } from '$lib/enums/available-locales';
+
+interface SiteMapRoute {
+	path: string;
+	priority: `${1 | 0}.${number}`;
+	changefreq: 'daily' | 'monthly' | 'yearly';
+}
+export async function GET() {
+	const baseUrl = BaseUrl;
+	const routes: SiteMapRoute[] = [
+		{ path: '', priority: '1.0', changefreq: 'daily' },
+		{ path: 'policies/cookies', priority: '0.8', changefreq: 'monthly' },
+		{ path: 'policies/privacy', priority: '0.8', changefreq: 'monthly' },
+		{ path: 'policies/terms', priority: '0.8', changefreq: 'monthly' },
+		{ path: 'accessibility-statement', priority: '0.7', changefreq: 'monthly' },
+		{ path: 'manage-cookies', priority: '0.6', changefreq: 'yearly' }
+	];
+	const sitemapUrls = routes
+		.map((route) => {
+			const englishUrl = route.path ? `${baseUrl}/${route.path}` : baseUrl;
+			const hebrewUrl = route.path
+				? `${baseUrl}/${AvailableLocals.Hebrew}/${route.path}`
+				: `${baseUrl}/${AvailableLocals.Hebrew}`;
+			return `
       <url>
-        <loc>https://my-brand.com</loc>
-        <image:image>
-          <image:loc>https://my-brand.com/images/logo.png</image:loc>
-        </image:image>
+        <loc>${englishUrl}</loc>
         <xhtml:link
-                 rel="alternate"
-                 href="https://my-brand.com"/>
-        <changefreq>daily</changefreq>
-        <priority>1</priority>
-      </url>
+          rel="alternate"
+          hreflang="${AvailableLocals.English_US}"
+          href="${englishUrl}"/>
+        <xhtml:link
+          rel="alternate"
+          hreflang="${AvailableLocals.Hebrew}"
+          href="${hebrewUrl}"/>
+        <changefreq>${route.changefreq}</changefreq>
+        <priority>${route.priority}</priority>
+      </url>`;
+		})
+		.join('');
+
+	return new Response(
+		`<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:xhtml="http://www.w3.org/1999/xhtml">
+      ${sitemapUrls}
     </urlset>`.trim(),
-    {			
-      headers: {
-        'Content-Type': 'application/xml',
-      },
-    },
-);}
+		{
+			headers: {
+				'Content-Type': 'application/xml'
+			}
+		}
+	);
+}
