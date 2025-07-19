46893c6a1cff0a7690134028d43217c8ed0e0cfa
Guy Lahav <guylahav@machine.local>
2025-07-19 19:05:45 +0300

    Added scrollable example page

---
+++ sveltekit+shadcn-v5/src/routes/[[lang]]/(application)/example/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import ResourceMarkdown from '$lib/components/resource-markdown/resource-markdown.svelte';
+	import { t } from '$lib/i18n';
+	import { title } from '$lib/stores';
+	title.set(t.get('common.example'));
+</script>
+
+{#each Array.from({ length: 10 })}
+	<ResourceMarkdown path="lorem-ipsum" />
+{/each}
