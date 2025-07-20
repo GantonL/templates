d22a31cf5815cce5655251f22f91bd309699be0f
Guy Lahav <guylahav@machine.local>
2025-07-20 08:33:16 +0300

    fixed full width issue in component

--- sveltekit+shadcn-v5/src/lib/components/shell/shell.svelte
+++ sveltekit+shadcn-v5/src/lib/components/shell/shell.svelte
@@ -11,7 +11,7 @@
 	<div class="flex h-svh w-full flex-col overflow-hidden">
 		<Header />
 		<div class="relative flex grow flex-col overflow-x-hidden overflow-y-auto">
-			<main class="m-auto flex flex-auto grow flex-col items-center gap-8 p-4">
+			<main class="flex flex-auto grow flex-col items-center gap-8 p-4">
 				{@render children?.()}
 			</main>
 			<Footer />
