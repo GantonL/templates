b3b91a941784ca0d4df62df4d74930555a3f413a
Guy Lahav <guylahav@machine.local>
2025-07-20 06:26:50 +0300

    Added tooltip example on side menu toggle button

--- sveltekit+shadcn-v5/src/lib/components/shell/header.svelte
+++ sveltekit+shadcn-v5/src/lib/components/shell/header.svelte
@@ -2,11 +2,21 @@
 	import { t } from '$lib/i18n';
 	import ThemeSwitcher from '../theme-switcher/theme-switcher.svelte';
 	import SidebarTrigger from '../ui/sidebar/sidebar-trigger.svelte';
+	import * as Tooltip from '../ui/tooltip';
 </script>
 
 <header class="grid w-full grid-cols-2 border-b p-2">
 	<div class="flex flex-row items-center gap-4">
-		<SidebarTrigger />
+		<Tooltip.Provider>
+			<Tooltip.Root>
+				<Tooltip.Trigger>
+					<SidebarTrigger />
+				</Tooltip.Trigger>
+				<Tooltip.Content>
+					<p>{$t('common.toggle_menu')}</p>
+				</Tooltip.Content>
+			</Tooltip.Root>
+		</Tooltip.Provider>
 		<h1 class="flex flex-row items-center gap-3 text-xl">
 			<a href="/">{$t('common.brand.name')}</a>
 		</h1>
