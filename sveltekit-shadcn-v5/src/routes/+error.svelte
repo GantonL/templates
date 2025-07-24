e6ba3ce999184893074f1e712d6c010e815489d7
Guy Lahav <guylahav@machine.local>
2025-07-24 18:22:28 +0300

    Support new metatags for error page

--- sveltekit-shadcn-v5/src/routes/+error.svelte
+++ sveltekit-shadcn-v5/src/routes/+error.svelte
@@ -1,12 +1,27 @@
 <script lang="ts">
 	import { page } from '$app/state';
+	import { getTitleTemplate } from '$lib/client/configurations/meta-tags';
 	import * as Alert from '$lib/components/ui/alert';
-	import { t } from '$lib/i18n';
-	import { title } from '$lib/stores';
+	import { locale, t } from '$lib/i18n';
+	import { metaTags } from '$lib/stores';
 	import { CircleAlert } from '@lucide/svelte';
+	import { onMount } from 'svelte';
+	import type { MetaTagsProps } from 'svelte-meta-tags';
 
 	const errorNumberMessage = t.get('common.error_number', { number: page?.status ?? 500 });
-	title.set(errorNumberMessage);
+
+	function setPageMetaTags() {
+		const title = errorNumberMessage;
+		const metaTagsObject = Object.freeze({
+			title,
+			titleTemplate: getTitleTemplate()
+		}) satisfies MetaTagsProps;
+		metaTags.set(metaTagsObject);
+	}
+
+	onMount(() => {
+		locale.subscribe(setPageMetaTags);
+	});
 </script>
 
 <div class="flex h-80 items-center justify-center">
