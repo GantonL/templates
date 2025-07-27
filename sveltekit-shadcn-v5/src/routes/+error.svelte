c8340e311d5c0bed62313cd4813bf39afabf8bef
Guy Lahav <guylahav@machine.local>
2025-07-27 08:46:37 +0300

    refine error boundry component

--- sveltekit-shadcn-v5/src/routes/+error.svelte
+++ sveltekit-shadcn-v5/src/routes/+error.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import { page } from '$app/state';
 	import { getTitleTemplate } from '$lib/client/configurations/meta-tags';
 	import * as Alert from '$lib/components/ui/alert';
+	import Button from '$lib/components/ui/button/button.svelte';
 	import { locale, t } from '$lib/i18n';
 	import { metaTags } from '$lib/stores';
 	import { CircleAlert } from '@lucide/svelte';
@@ -22,11 +24,19 @@
 	onMount(() => {
 		locale.subscribe(setPageMetaTags);
 	});
+
+	function goBack() {
+		window.history.back();
+	}
+
+	function goHome() {
+		goto('/');
+	}
 </script>
 
-<div class="flex h-80 items-center justify-center">
+<div class="flex h-svh items-center justify-center">
 	{#if page?.error}
-		<div class="flex flex-col">
+		<div class="flex flex-col gap-2">
 			<Alert.Root variant="destructive" class="border-destructive/50">
 				<CircleAlert />
 				<Alert.Title>{errorNumberMessage}</Alert.Title>
@@ -34,6 +44,14 @@
 					<p>{$t(`common.status_code_error.${page.status}.description`)}</p>
 				</Alert.Description>
 			</Alert.Root>
+			<section class="flex w-full flex-row gap-2">
+				<Button class="grow" variant="outline" onclick={goHome}>
+					{$t('common.home')}
+				</Button>
+				<Button class="grow" variant="outline" onclick={goBack}>
+					{$t('common.back')}
+				</Button>
+			</section>
 		</div>
 	{/if}
 </div>
