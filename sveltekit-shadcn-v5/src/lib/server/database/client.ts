b4b59fc2fd2ba45762f78950b7cd427685c714ba
Guy Lahav <guylahav@machine.local>
2025-08-23 20:19:09 +0300

    fix: use dynamic env vars in runtime instead of build time to resolve DATABSE_URL env var in cloudflare

--- sveltekit-shadcn-v5/src/lib/server/database/client.ts
+++ sveltekit-shadcn-v5/src/lib/server/database/client.ts
@@ -1,9 +1,9 @@
-import { DATABASE_URL } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 import { sql } from 'drizzle-orm';
 
-export const client = postgres(DATABASE_URL, { prepare: false });
+export const client = postgres(env.DATABASE_URL, { prepare: false });
 export const db = drizzle(client);
 
 export const checkConnection = async () => {
