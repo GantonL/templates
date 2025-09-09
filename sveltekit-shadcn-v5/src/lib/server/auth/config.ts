1d0ec89137314098af78c47f772a8841e34faa63
Guy Lahav <guylahav@machine.local>
2025-09-09 22:05:45 +0300

    working example with google

--- sveltekit-shadcn-v5/src/lib/server/auth/config.ts
+++ sveltekit-shadcn-v5/src/lib/server/auth/config.ts
@@ -2,6 +2,8 @@ import { db } from '$lib/server/database/client';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { betterAuth } from 'better-auth';
 import { account, session, user, verification } from '../database/schemas/auth';
+import { sveltekitCookies } from 'better-auth/svelte-kit';
+import { getRequestEvent } from '$app/server';
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
@@ -15,5 +17,12 @@ export const auth = betterAuth({
 	}),
 	emailAndPassword: {
 		enabled: true
-	}
+	},
+	socialProviders: {
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+		}
+	},
+	plugins: [sveltekitCookies(getRequestEvent)]
 });
--- sveltekit-shadcn-v5/src/lib/server/auth/handle.ts
+++ sveltekit-shadcn-v5/src/lib/server/auth/handle.ts
@@ -3,5 +3,12 @@ import { auth } from './config';
 import { building } from '$app/environment';
 
 export async function handle({ event, resolve }): Promise<Response> {
+	const session = await auth.api.getSession({
+		headers: event.request.headers
+	});
+	if (session) {
+		event.locals.session = session.session;
+		event.locals.user = session.user;
+	}
 	return svelteKitHandler({ event, resolve, auth, building });
 }
